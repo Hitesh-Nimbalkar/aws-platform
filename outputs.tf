60580b7d526bdfb203914ece57ab413d7d620e8c
Hitesh Nimbalkar <nimbalkarhitesh@gmail.com>
2025-09-06 00:34:08 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -8,6 +8,6 @@ output "monitoring_private_subnet_ids" {
 output "monitoring_security_group_id" {
   value = module.networking.security_group_id
 }
-output "platform_ecr_repository_url" {
-  value = aws_ecr_repository.platform.repository_url
-}
+# output "platform_ecr_repository_url" {
+#   value = aws_ecr_repository.platform.repository_url
+# }
