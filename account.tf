71c842fa2f8859751eb2cc03c38b185a37bd5d63
Hitesh Nimbalkar <nimbalkarhitesh@gmail.com>
2025-09-05 23:39:21 +0530

    Update account.tf

--- account.tf
+++ account.tf
@@ -1,14 +1,9 @@
-
-data "aws_vpc" "default" {
-  default = true
-}
-# Fetch AWS account ID dynamically
-data "aws_caller_identity" "current" {}
-# Fetch default VPC for the account/region
 data "aws_vpc" "default" {
     default = true
 }
-# Consolidated locals for account-wide settings
+
+data "aws_caller_identity" "current" {}
+
 locals {
     organization    = "dummy"
     project         = "dummy-project"
@@ -17,6 +12,7 @@ locals {
     account_id      = data.aws_caller_identity.current.account_id
     default_vpc_id  = data.aws_vpc.default.id
 }
+
 output "default_vpc_id" {
     value = local.default_vpc_id
 }
