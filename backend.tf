2a5831a417b7a4d9a88208938c014191c444a8e9
Hitesh Nimbalkar <nimbalkarhitesh@gmail.com>
2025-09-05 23:38:00 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -8,36 +8,3 @@ terraform {
     encrypt        = true
   }
 }
-
-resource "aws_s3_bucket" "tf_state" {
-  bucket = var.tf_state_bucket
-  force_destroy = false
-  tags = var.common_tags
-}
-resource "aws_s3_bucket_versioning" "tf_state_versioning" {
-  bucket = aws_s3_bucket.tf_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-# TERRAFORM STATE BACKEND CONFIGURATION
-# =============================================================================
-variable "tf_state_bucket" {
-  description = "The name of the S3 bucket for storing the Terraform state file."
-  type        = string
-}
-variable "tf_state_key" {
-  description = "The path/key in the S3 bucket for the Terraform state file."
-  type        = string
-  default     = "platform/terraform.tfstate"
-}
-variable "tf_state_region" {
-  description = "The AWS region for the S3 bucket used for the Terraform state file."
-  type        = string
-}
-variable "tf_state_dynamodb_table" {
-  description = "The name of the DynamoDB table for state locking."
-  type        = string
-  default     = null
-}
