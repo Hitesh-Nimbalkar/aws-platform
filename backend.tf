cf78022ac6e68a40bba96dd2308baabcdc708c80
Hitesh Nimbalkar <nimbalkarhitesh@gmail.com>
2025-09-05 23:41:42 +0530

    q

--- backend.tf
+++ backend.tf
@@ -21,23 +21,4 @@ resource "aws_s3_bucket_versioning" "tf_state_versioning" {
   }
 }
 
-# TERRAFORM STATE BACKEND CONFIGURATION
-# =============================================================================
-variable "tf_state_bucket" {
-  description = "The name of the S3 bucket for storing the Terraform state file."
-  type        = string
-}
-variable "tf_state_key" {
-  description = "The path/key in the S3 bucket for the Terraform state file."
-  type        = string
-  default     = "platform/terraform.tfstate"
-}
-variable "tf_state_region" {
-  description = "The AWS region for the S3 bucket used for the Terraform state file."
-  type        = string
-}
-variable "tf_state_dynamodb_table" {
-  description = "The name of the DynamoDB table for state locking."
-  type        = string
-  default     = null
-}
+
