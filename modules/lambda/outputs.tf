62615b5a342327ce2f7b9508a12817a537491a7e
Hitesh Nimbalkar <nimbalkarhitesh@gmail.com>
2025-09-06 00:21:40 +0530

    Update outputs.tf

--- modules/lambda/outputs.tf
+++ modules/lambda/outputs.tf
@@ -19,22 +19,22 @@ output "lambda_function_qualified_arn" {
   description = "The qualified ARN of the Lambda function"
   value       = aws_lambda_function.this.qualified_arn
 }
+output "lambda_function_arn" {
+  value = [for f in aws_lambda_function.this : f.arn]
+}
+output "lambda_function_name" {
+  value = [for f in aws_lambda_function.this : f.function_name]
+}
+output "lambda_function_invoke_arn" {
+  value = [for f in aws_lambda_function.this : f.invoke_arn]
+}
+output "lambda_function_qualified_arn" {
+  value = [for f in aws_lambda_function.this : f.qualified_arn]
+}
 output "lambda_function_version" {
-  description = "Latest published version of the Lambda function"
-  value       = aws_lambda_function.this.version
+  value = [for f in aws_lambda_function.this : f.version]
 }
-output "lambda_role_arn" {
-  description = "The ARN of the IAM role used by the Lambda function"
-  value       = aws_iam_role.lambda_role.arn
-}
-output "lambda_role_name" {
-  description = "The name of the IAM role used by the Lambda function"
-  value       = aws_iam_role.lambda_role.name
-}
-output "lambda_log_group_name" {
-  description = "The name of the CloudWatch log group for the Lambda function"
-  value       = aws_cloudwatch_log_group.lambda_log_group.name
-}
+
 output "lambda_log_group_arn" {
   description = "The ARN of the CloudWatch log group for the Lambda function"
   value       = aws_cloudwatch_log_group.lambda_log_group.arn
