e02844477ea29f4d373d5cdade8dee16dffde175
Hitesh Nimbalkar <nimbalkarhitesh@gmail.com>
2025-09-06 11:04:18 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -9,30 +9,5 @@ terraform {
   }
 }
 
-resource "aws_s3_bucket" "tf_state" {
-  bucket = "msumani-terraform-state"
-  force_destroy = false
-  tags = var.common_tags
-}
-resource "aws_s3_bucket_versioning" "tf_state_versioning" {
-  bucket = aws_s3_bucket.tf_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-# DynamoDB table for Terraform state locking
-resource "aws_dynamodb_table" "tf_state_lock" {
-  name         = "msumani-terraform-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-
-  tags = var.common_tags
-}
 
 
